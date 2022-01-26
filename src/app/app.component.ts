a185a4ae56cfac205855db687ca36975d89354be
Saqib-ghouri11 <39990883+Saqib-ghouri11@users.noreply.github.com>
2022-01-26 14:35:21 +0500

    TDF Validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
   options=['angular','react','vue']
   title = 'angular-forms';
 
-  userData:User = new User('saqib','abc@gmail.com',3101212123,{houseNo:'g/123',city:'hyd',state:'Pak',postalCode:112233,},'angular','morning',true);
+  userData:User = new User('','abc@gmail.com',3101212123,{houseNo:'g/123',city:'hyd',state:'Pak',postalCode:112233,},'angular','morning',true);
 
 
 }
