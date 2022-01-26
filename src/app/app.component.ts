018a8f76dd11839a249ed788c72453f306338fa8
Saqib-ghouri11 <39990883+Saqib-ghouri11@users.noreply.github.com>
2022-01-26 15:56:29 +0500

    custom select validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,15 @@ import { User } from './classes/user';
 export class AppComponent {
   options=['angular','react','vue']
   title = 'angular-forms';
+  isSelectInvalid=false;
 
-  userData:User = new User('','abc@gmail.com',3101212123,{houseNo:'g/123',city:'hyd',state:'Pak',postalCode:112233,},'angular','morning',true);
-
+  userData:User = new User('','abc@gmail.com',3101212123,{houseNo:'g/123',city:'hyd',state:'Pak',postalCode:112233,},'default','morning',true);
+  validateSelect(value:any){
+    if(value=='default'){
+      this.isSelectInvalid=true;
+    }else{
+      this.isSelectInvalid=false;
+    }
+  }
 
 }
