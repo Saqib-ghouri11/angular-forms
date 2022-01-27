1b9d62834338cb544a0243a5a6d57d8f3c0fa003
Saqib-ghouri11 <39990883+Saqib-ghouri11@users.noreply.github.com>
2022-01-27 16:07:36 +0500

    Load Api Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactive-forms';
+
+  reactiveFormModel = new FormGroup({
+    username:new FormControl('SaqibAli08'),
+    password:new FormControl(''),
+    confirmPassword:new FormControl(''),
+    address:new FormGroup({
+      city:new FormControl(''),
+      state:new FormControl(''),
+      postalCode:new FormControl(''),
+    }),
+  });
+
+  loadData(){
+    //here setValues is restricted to set all field. 
+    //we can use patchValues if we want to set only few fields
+    this.reactiveFormModel.setValue({
+      username:"SaqibAli08",
+      password:"123456",
+      confirmPassword:"123456",
+      address:{
+        city:"city",
+        state:"state",
+        postalCode:"123456"
+      }
+    });
+  }
+
 }
